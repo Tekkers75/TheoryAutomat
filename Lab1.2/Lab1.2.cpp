a6244eff0d537db10e697bac77a60f8a7dfbfaff
Tekkers75 <Tekkers75@gmail.com>
2024-02-20 22:40:25 +0900

    5,6 labs

--- Lab1.2/Lab1.2.cpp
+++ Lab1.2/Lab1.2.cpp
@@ -6,11 +6,11 @@ using namespace std;
 int main()
 {
 	setlocale(LC_ALL, "rus");
-	
-	
+
+
 	cout << "Первая лабораторная работа." << endl;
 	cout << "Задание 2.a" << endl;
-	list<Rule> dict = 
+	list<Rule> dict =
 	{
 		 Rule("S", "aaCFD"),
 		 Rule("AD", "D"),
@@ -24,26 +24,32 @@ int main()
 	};
 	PrintRules(dict);
 	FormalLanguage fl1(dict);
+	cout << "Левосторонний вывод из рандомных выбранных правил" << endl;
+	cout << fl1.OutputLeft() << endl;
+	cout << "Минимальная цепочка: " << endl;
 	cout << "Цепочка " + fl1.Translate("S") << endl;
 	cout << "Язык:  L = { a^n | n > 0 }" << endl;
 
 	cout << "Задание 2.б" << endl;
-	dict =
+	list<Rule> dict2 =
 	{
-		 Rule("S", "A|"),  
-		 Rule("S", "B|"),
+		 Rule("S", "A$"),
+		 Rule("S", "B$"),
 		 Rule("A", "a"),
 		 Rule("A", "Ba"),
-		 Rule("b", "b"),
-		 Rule("b", "Bb"),
-		 Rule("b", "Ab"),
+		 Rule("B", "b"),
+		 Rule("B", "Bb"),
+		 Rule("B", "Ab"),
 	};
-	PrintRules(dict);
-	FormalLanguage fl2(dict);
+	PrintRules(dict2);
+	FormalLanguage fl2(dict2);
+	cout << "Левосторонний вывод из рандомных выбранных правил" << endl;
+	cout << fl2.OutputLeft() << endl;
+	cout << "Минимальная цепочка: " << endl;
 	cout << "Цепочка " + fl2.Translate("S") << endl;
-	cout << "Язык: L = { (a|)^n | n>0 }" << endl;
+	cout << "Язык: L = { a^n,b^m $| n>0, m>0 }" << endl;
 
-	
+
 
 	return 0;
 }
